cbdde4bfb0a16a40de202f4c44a12c3e9a86c98d
dzimiks <vana997@gmail.com>
2023-07-31 00:46:15 +0200

    feat: add empty tenderly handler

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,25 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Page() {
   return (
     <div>
       <Head>
-        <meta name="og:title" content="Vercel Edge Network" />
-        <meta name="og:description" content="Vercel Edge Network" />
+        <meta name="og:title" content="Transaction Trace by Tenderly.co" />
+        <meta name="og:description" content="Get transaction trace in one click. More than 30 EVM-based networks supported." />
         <meta
           name="og:image"
           content={
-            // Because OG images must have a absolute URL, we use the
+            // Because OG images must have an absolute URL, we use the
             // `VERCEL_URL` environment variable to get the deployment’s URL.
             // More info:
             // https://vercel.com/docs/concepts/projects/environment-variables
             `${
               process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
+            }/api/tenderly`
           }
         />
       </Head>
-      <h1>A page with Open Graph Image.</h1>
+      <h1>Transaction Trace</h1>
     </div>
-  )
+  );
 }
