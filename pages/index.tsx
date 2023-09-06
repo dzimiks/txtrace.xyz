2372cf0e46c275fd099babca6feee33b2a2344f9
dzimiks <vana997@gmail.com>
2023-09-07 00:35:09 +0200

    fix typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <meta name="og:description"
               content="Get transaction trace in one click. More than 30 EVM-based networks supported." />
       </Head>
-      <h1>Transaction Trace Simlified.</h1>
+      <h1>Transaction Trace Simplified.</h1>
     </div>
   );
 }
